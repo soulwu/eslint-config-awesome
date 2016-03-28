5c5829df970b0e9fe01be7e7f7ae2b84405675ec
陶淑勇 <taoshuyong@noahwm.com>
2016-03-28 18:30:09 +0800

    补充额外的规则

--- base.js
+++ base.js
@@ -201,6 +201,99 @@ module.exports = {
         words: true,
         nonwords: false
       }
-    ]
+    ],
+    'no-extra-parens': [2, 'functions'],
+    'no-shadow-restricted-names': 2,
+    'no-array-constructor': 2,
+    // treat var statements as if they were block scoped
+    'block-scoped-var': 2,
+    // require return statements to either always or never specify values
+    'consistent-return': 2,
+    // require default case in switch statements
+    'default-case': 2,
+    // encourages use of dot notation whenever possible
+    'dot-notation': [2, {allowKeywords: true}],
+    // enforces consistent newlines before or after dots
+    'dot-location': [2, 'property'],
+    // require the use of === and !==
+    eqeqeq: 2,
+    // make sure for-in loops have an if statement
+    'guard-for-in': 2,
+    // disallow the use of alert, confirm, and prompt
+    'no-alert': 2,
+    // disallow use of arguments.caller or arguments.callee
+    'no-caller': 2,
+    // disallow division operators explicitly at beginning of regular expression
+    'no-div-regex': 2,
+    // disallow else after a return in an if
+    'no-else-return': 2,
+    // disallow use of eval()
+    'no-eval': 2,
+    // disallow adding to native types
+    'no-extend-native': 2,
+    // disallow unnecessary function binding
+    'no-extra-bind': 2,
+    // disallow fallthrough of case statements
+    'no-fallthrough': 2,
+    // disallow the use of leading or trailing decimal points in numeric literals
+    'no-floating-decimal': 2,
+    // disallow the type conversions with shorter notations
+    'no-implicit-coercion': [2, {
+      boolean: true,
+      number: true,
+      string: true,
+      allow: ['!!']
+    }],
+    // disallow use of eval()-like methods
+    'no-implied-eval': 2,
+    // disallow this keywords outside of classes or class-like objects
+    'no-invalid-this': 2,
+    // disallow usage of __iterator__ property
+    'no-iterator': 2,
+    // disallow use of labeled statements
+    'no-labels': 2,
+    // disallow unnecessary nested blocks
+    'no-lone-blocks': 2,
+    // disallow creation of functions within loops
+    'no-loop-func': 2,
+    // disallow use of multiple spaces
+    'no-multi-spaces': 2,
+    // disallow use of multiline strings
+    'no-multi-str': 2,
+    // disallow reassignments of native objects
+    'no-native-reassign': 2,
+    // disallow use of new operator when not part of the assignment or comparison
+    'no-new': 2,
+    // disallow use of new operator for Function object
+    'no-new-func': 2,
+    // disallows creating new instances of String,Number, and Boolean
+    'no-new-wrappers': 2,
+    // disallow use of (old style) octal literals
+    'no-octal': 2,
+    // disallow use of octal escape sequences in string literals, such as
+    // var foo = 'Copyright \251';
+    'no-octal-escape': 2,
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation
+    // rule: http://eslint.org/docs/rules/no-param-reassign.html
+    'no-param-reassign': [2, {props: true}],
+    // disallow usage of __proto__ property
+    'no-proto': 2,
+    // disallow use of `javascript:` urls.
+    'no-script-url': 2,
+    // restrict what can be thrown as an exception
+    'no-throw-literal': 2,
+    // disallow unnecessary .call() and .apply()
+    'no-useless-call': 2,
+    // disallow use of void operator
+    'no-void': 2,
+    // disallow use of the with statement
+    'no-with': 2,
+    // requires to declare all vars on top of their containing scope
+    'vars-on-top': 2,
+    // require immediate function invocation to be wrapped in parentheses
+    'wrap-iife': [2, 'inside'],
+    // require or disallow Yoda conditions
+    yoda: 2
   }
 };
