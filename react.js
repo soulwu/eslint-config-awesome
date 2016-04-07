40147548a41cbd7cd11258ccddbdebacc7bca2c6
吴静波 <wujingbo@noahwm.com>
2016-04-07 18:42:28 +0800

    添加react/jsx相关规则

---
+++ react.js
@@ -0,0 +1,35 @@
+module.exports = {
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    __dirname + '/browser.js'
+  ],
+  rules: {
+    'react/display-name': [2, {ignoreTranspilerName: true}],
+    'react/no-deprecated': 2,
+    'react/no-unknown-property': 2,
+    'react/prefer-es6-class': [2, 'always'],
+    'react/prop-types': 2,
+    'react/react-in-jsx-scope': 2,
+    'react/self-closing-comp': 2,
+    'react/sort-comp': 2,
+    'react/sort-prop-types': 2,
+    'react/wrap-multilines': 2,
+    'react/jsx-curly-spacing': [2, 'never'],
+    'react/jsx-equals-spacing': [2, 'never'],
+    'react/jsx-handler-names': 2,
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-indent': [2, 2],
+    'react/jsx-no-bind': [2, {ignoreRefs: true}],
+    'react/jsx-uses-react': 2,
+    'react/jsx-no-duplicate-props': 2,
+    'react/jsx-no-undef': 2,
+    'react/jsx-pascal-case': 2,
+    'react/jsx-space-before-closing': [2, 'always'],
+    'react/jsx-uses-vars': 2
+  }
+};
